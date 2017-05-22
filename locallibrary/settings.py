1d4650c70460f8f30f7455a0e54fe57bba339aa9
Attila O. Szabó <oszabo@192.168.88.226>
2017-05-22 20:50:26 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['https://fathomless-waters-51242.herokuapp.com','http://127.0.0.1:8000/catalog/']
+ALLOWED_HOSTS =['fathomless-waters-51242.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
